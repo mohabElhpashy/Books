cd4ef0f1fc004576cc50a078724dd6c58ea42557
mohabelhbashy <mohab.hafez332@gmail.com>
2021-01-29 23:45:54 +0200

    add new updats

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -3,26 +3,61 @@ import { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import book from "../../imgs/books.jpg";
 import test from "../../imgs/five.jpg";
+// import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
+import Modal from "react-awesome-modal";
 
+// import x from "../../imgs/starcolored.jpg";
 // import pic from "../../imgs/Capture.PNG";
 
 import "./Content.css";
 import { Form, Button, Navbar, FormControl, Card } from "react-bootstrap";
 
 class Content extends Component {
-  state = {
-    flag: false,
-    tittle: [
-      "The Love Square",
-      "The Love Square",
-      "The Love Square",
-      "The Love Square",
-      "The Love Square",
-      "The Love Square",
-      "The Love Square",
-    ],
-    card_text: ["one", "two", "three", "four", "five", "six", "seven"],
-  };
+  constructor() {
+    super();
+    this.state = {
+      show: false,
+      flag: false,
+      visible: false,
+      index: 0,
+      data: [
+        {
+          tittle: ["The Love Square"],
+          card_index: ["one"],
+          rate: ["1", "2", "3", "4", "5"],
+        },
+        {
+          tittle: ["how to make a deal"],
+          card_index: ["one"],
+          rate: ["1", "2", "3", "4", "5"],
+        },
+        {
+          tittle: ["how to make a deal 2"],
+          card_index: ["one"],
+          rate: ["1", "2", "3", "4", "5"],
+        },
+        {
+          tittle: ["how to make a deal 3"],
+          card_index: ["one"],
+          rate: ["1", "2", "3", "4", "5"],
+        },
+        {
+          tittle: ["how to make a deal 4"],
+          card_index: ["one"],
+          rate: ["1", "2", "3", "4", "5"],
+        },
+        {
+          tittle: ["how to make a deal 5"],
+          card_index: ["one"],
+          rate: ["1", "2", "3", "4", "5"],
+        },
+      ],
+    };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
   showDivinfo = () => {
     if (this.state.flag === false) {
       document.getElementById("divInf").style.height = "360px";
@@ -33,8 +68,28 @@ class Content extends Component {
       this.setState({ flag: false });
     }
   };
+  ratefun = (index) => {
+    // document.getElementById(buttonindex).style.backgroundImage =
+    //   "url(/images/starcolored.jpg)";
+  };
+
+  openModal = (index) => {
+    this.setState({
+      visible: true,
+      index: index,
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
   render() {
-    const mapping = this.state.tittle.map((index, tittlee) => {
+    console.log("===>", this.state.data[5].tittle);
+
+    const mapping = this.state.data.map((dta, index) => {
       return (
         <div className="divcontent">
           <Card style={{ width: "18rem" }}>
@@ -43,30 +98,51 @@ class Content extends Component {
               src={test}
               style={{ width: "288px", height: "250px" }}
               id="card"
+              onClick={this.openModal.bind(this, index)}
             />
             <Card.Body>
-              <Card.Title>{index}</Card.Title>
+              <Card.Title>{dta.tittle}</Card.Title>
               <hr />
               <Card.Text>
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </Card.Text>
-              <Button variant="danger">+</Button>
-              <span class="rate-this-stars">
-                <h5>Rate this page</h5>
-                <ol class="rate-this-stars-list">
-                  <li class="star" value="5"></li>
-                  <li class="star" value="4"></li>
-                  <li class="star" value="3"></li>
-                  <li class="star" value="2"></li>
-                  <li class="star" value="1"></li>
-                </ol>
-              </span>
+              <Button
+                // onClick={this.cardindex.bind(this, tittle)}
+                variant="danger"
+                style={{ position: "relative", left: "200px" }}
+                // onClick={() => this.openModal(this, index)}
+              >
+                +
+              </Button>
+              {dta.rate.map((rate, index) => {
+                return (
+                  <div class="rate-this-stars">
+                    <button></button>
+                  </div>
+                );
+              })}
             </Card.Body>
+            <Modal
+              className="modal"
+              visible={this.state.visible}
+              width="50%"
+              height="500px"
+              effect="fadeInUp"
+              style={{ opacity: "0.2" }}
+              onClickAway={() => this.closeModal()}
+            >
+              <div>
+                <img src={test} alt="book" />
+                <h1>{this.state.data[this.state.index].tittle}</h1>
+                <p></p>
+              </div>
+            </Modal>
           </Card>
         </div>
       );
     });
+
     return (
       <div>
         <Navbar bg="dark" variant="dark">
@@ -102,7 +178,7 @@ class Content extends Component {
           <hr />
           <h5
             style={{
-              letterSpacing: "10px",
+              // letterSpacing: "10px",
               position: "absolute",
               top: "325px",
               left: "100px",
@@ -119,3 +195,12 @@ class Content extends Component {
 }
 
 export default Content;
+
+/* <Popup
+          width="200px"
+          height="500px"
+          trigger={<button> Trigger</button>}
+          position="right center"
+        >
+          <div>Popup content here !!</div>
+        </Popup> */
